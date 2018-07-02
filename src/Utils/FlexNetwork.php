6d00c60a0ee52c38ba6b401d6753ef8dc549df57
danda <danda@osc.co.cr>
2018-07-02 17:17:39 +0800

    add FlexNetwork class to add networks from coinparams.json

---
+++ src/Utils/FlexNetwork.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Utils;
+
+use BitWasp\Bitcoin\Network\Network;
+use BitWasp\Bitcoin\Script\ScriptType;
+use coinParams\coinParams;
+
+class FlexNetwork extends Network {
+    
+    protected $base58PrefixMap;
+    protected $bip32PrefixMap;
+    protected $bip32ScriptTypeMap;
+    protected $signedMessagePrefix;
+    protected $p2pMagic;
+    
+    function __construct($coin) {
+        $network = 'main';
+        if(strstr($coin, '-')) {
+            list($coin, $network) = explode('-', $coin);
+        }
+        echo "coin: $coin, net: $network\n";
+        
+        $params = coinParams::get_coin_network($coin, $network);
+        
+        $this->base58PrefixMap = [
+            self::BASE58_ADDRESS_P2PKH => self::dh(@$params['prefixes']['public']),
+            self::BASE58_ADDRESS_P2SH => self::dh(@$params['prefixes']['scripthash']),
+            self::BASE58_WIF => self::dh(@$params['prefixes']['private']),
+        ];
+        
+        $this->bip32PrefixMap = [
+            // https://github.com/zcash/zcash/blob/master/src/chainparams.cpp#L146-L147
+            self::BIP32_PREFIX_XPUB => self::th(@$params['prefixes']['bip32']['public'], true),
+            self::BIP32_PREFIX_XPRV => self::th(@$params['prefixes']['bip32']['private'], true),
+        ];
+    
+        $this->bip32ScriptTypeMap = [
+            self::BIP32_PREFIX_XPUB => ScriptType::P2PKH,
+            self::BIP32_PREFIX_XPRV => ScriptType::P2PKH,
+        ];
+    
+        $this->signedMessagePrefix = $params['message_magic'];
+    
+        $this->p2pMagic = self::th(@$params['protocol']['magic']);
+        
+//        print_r($this); exit;
+        
+    }
+    
+    /** incoming values look like 0x1ec
+     *  but bitwasp lib expects them like
+     *  01ec or ec instead.  this method drops the 0x
+     *  and prepends 0 if necessary to make length an even number.
+     */
+    static private function th($hex, $prepend_zero = false) {
+        $hex = substr($hex, 2);
+        $pre = strlen($hex) % 2 == 0 ? '' : '0';
+        return $pre . $hex;
+    }
+    
+    static private function dh($dec, $prepend_zero = false) {
+        $hex = dechex($dec);
+        $pre = strlen($hex) % 2 == 0 ? '' : '0';
+        return $pre . $hex;
+    }
+    
+    
+    
+}
+
--- src/Utils/NetworkCoinFactory.php
+++ src/Utils/NetworkCoinFactory.php
@@ -11,50 +11,31 @@ namespace App\Utils;
 
 use BitWasp\Bitcoin\Network\Network;
 use BitWasp\Bitcoin\Network\NetworkFactory;
+use coinParams\coinParams;
 
 class NetworkCoinFactory extends Network
 {
     public static function getNetworkCoinsList() {
-        return [
-            // alpha, by symbol.
-            'bch'          => 'Bitcoin Cash',
-            'bch-testnet'  => 'Bitcoin Cash Testnet',
-            'btc'          => 'Bitcoin',
-            'btc-testnet'  => 'Bitcoin Testnet',
-            'btc-regtest'  => 'Bitcoin Regtest',
-            'dash'         => 'Dash',
-            'dash-testnet' => 'Dash Testnet',
-            'doge'         => 'Dogecoin',
-            'doge-testnet' => 'Dogecoin Testnet',
-            'ltc'          => 'Litecoin',
-            'ltc-testnest' => 'Litecoin Testnet',
-            'via'          => 'Viacoin',
-            'via-testnest' => 'Viacoin Testnet',
-            'zec'          => 'ZCash',
-        ];
+        
+        $coins = coinParams::get_all_coins();
+        
+        $list = [];
+        foreach($coins as $sym => $c) {
+            foreach($c as $net => $info) {
+                if(!@$info['prefixes']['bip32']['public'] ||
+                   !@$info['prefixes']['bip32']['private'] ) {
+                    continue;
+                }
+                $suffix = $net == 'main' ? '' : "-$net";
+                $symbol = $sym . $suffix;
+                $list[$symbol] = $info['name'];
+            }
+        }
+        return $list;
     }
     
     public static function getNetworkCoinInstance($coin)
     {
-        switch($coin)
-        {
-            case 'bch'          : return NetworkFactory::bitcoin();  // bch is a special case and uses bitcoin network.
-            case 'bch-testnet'  : return NetworkFactory::bitcoinTestnet();
-            case 'btc'          : return NetworkFactory::bitcoin();
-            case 'btc-testnet'  : return NetworkFactory::bitcoinTestnet();
-            case 'btc-regtest'  : return NetworkFactory::bitcoinRegtest();
-            case 'dash'         : return NetworkFactory::dash();
-            case 'dash-testnet' : return NetworkFactory::dashTestnet();
-            case 'doge'         : return NetworkFactory::dogecoin();
-            case 'doge-testnet' : return NetworkFactory::dogecoinTestnet();
-            case 'ltc'          : return NetworkFactory::litecoin();
-            case 'ltc-testnet'  : return NetworkFactory::litecoinTestnet();
-            case 'via'          : return NetworkFactory::viacoin();
-            case 'via-testnet'  : return NetworkFactory::viacoinTestnet();
-            case 'zec'          : return NetworkFactory::zcash();
-                
-            default:
-                throw new Exception("Coin '$coin' is unrecognized");
-        }
+        return new FlexNetwork($coin);
     }
 }
